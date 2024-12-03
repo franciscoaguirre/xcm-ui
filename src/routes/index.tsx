7312783c59d08191cecd5d96d534426eeb6f2d10
Tiago Bandeira <tiago.bandeira@parity.io>
2024-12-03 15:55:43 +0100

    sidebar's first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
 import { A } from "@solidjs/router";
 import Counter from "~/components/Counter";
+import { AppSidebar } from "~/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        </main>
+      </SidebarProvider>
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
       <Counter />
       <p class="mt-8">
